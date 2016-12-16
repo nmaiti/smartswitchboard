23b063c79f215250d6da0066b6c3c96fa111135a
Vikas Gaikwad <vksgaikwad3@gmail.com>
2016-12-16 18:45:42 +0530

    removed case(555): ble on connect response code , no need to implemnt it

--- Software/main_8KeyRelayModule/main_8keyRelayModule/main_8keyRelayModule.ino
+++ Software/main_8KeyRelayModule/main_8keyRelayModule/main_8keyRelayModule.ino
@@ -14,21 +14,17 @@
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include <HBT.h>
-#include <EEPROM.h>
-#include <Wire.h>   // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+//#include <EEPROM.h>
+//#include <Wire.h>   // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include "QTouch.h"
-#include "BLE.h"
-
+//#include "BLE.h"
 //#include "RTClib.h"
-
 
 
 //RTC_DS1307 rtc;
 HBT myHBT;          // Object Variables
 QTouch myTouch;  
-BLE getStatus;
-
-//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//BLE getStatus;
 
 
 //Global Variables
@@ -36,13 +32,6 @@ BLE getStatus;
 int Pin_Type;    //  Stores Pin Type  
 int Pin_No;      //  Stores Pin Number
 int Pin_Value;   //  Stores Pin Value 
-
-/// RF Modules Output pins as Input to MCU PB0 - PB3
-
-#define RFD0  14    //D14
-#define RFD1  16    //D16(RFD2)
-#define RFD2  17    //D17(RFD3)
-#define RFD3  15    //D15(RFD1)
 
 /// Using PinChange Interrupt0
 
@@ -53,7 +42,8 @@ bool fon = false,foff= false,up_in = false,mstr_on =false;
 volatile uint8_t level = 0;
 int switch_num=0;
 
-
+//void(*resetFunc)(void) = 0;   //declare reset function at address 0
+//resetFunc();  //call reset
 
 void setup()
 {
@@ -62,7 +52,7 @@ void setup()
 
   myHBT.enterCommandMode();
 
-  myHBT.setName("HBT_Test");
+  myHBT.setName("HALL");
   myHBT.setTransmissionPower(Six);
   myHBT.setWorkRole(SLAVE);
   myHBT.setAuth(OPEN);
@@ -87,23 +77,15 @@ void setup()
     //rtc.adjust(DateTime(2016, 2, 12, 17, 25, 0));
   }
   */  
-/// Pin Change Interrupt 
-/// define the pins
 
-pinMode(RFD0,INPUT);
-pinMode(RFD1,INPUT);
-pinMode(RFD2,INPUT);
-pinMode(RFD3,INPUT);
-
-//DDRB = 0b11110000 ;   // PB0 - PB3 RF Input Pins
-
-sei();      //Enable Global Interrupt
-
+//sei();      //Enable Global Interrupt
 }  
 
 void loop()
 {  
-        
+   // Note: please add Pull down resistor on RF inputs pins ***************************************
+   
+  // Curr_RFStatus = myTouch.readRF() & 0x0F;      //Read RF Status     
    touch_input = myTouch.readCapsense() & 0x7F;  //Read capsense values
    
    //Serial.println(touch_input,HEX);
@@ -114,93 +96,96 @@ void loop()
       Pin_No = myHBT.parseInt();
       Pin_Value = myHBT.parseInt();
 
-      Serial.print(Pin_Type);
-      Serial.print(",");
-      Serial.print(Pin_No);
-      Serial.print(","); 
-      Serial.println(Pin_Value);
-      Serial.print("I am Here");
+      //Serial.print(Pin_Type);
+      //Serial.print(",");
+      //Serial.print(Pin_No);
+      //Serial.print(","); 
+      //Serial.println(Pin_Value);
+      //Serial.print("I am Here");
      // myTouch.LightControl(Pin_No,Pin_Value);
-    if(myHBT.read() =='H')                      // detects endline character "H"
-    {
-        Serial.print("I am in if.read()");
-        //if Pin Type is 0 means Digital Output      
-       switch(Pin_Type)
-       {
-          case(0):
-                  if((Pin_No == Level1 || Pin_No == Level2 || Pin_No == Level3 || Pin_No == Level4) && (Pin_Value == 1))    // Any one Level is selected and Value set to HIGH 
-                  { Serial.print("In Case 0");
-                    if(Pin_No == Level1 && Pin_Value ==1){ Serial.print("Level 1");myTouch.FAN_ON();}       //  Level 1 ON 
-                    if(Pin_No == Level2 && Pin_Value ==1){ Serial.print("Level 2");myTouch.Level2_ON();}    //  Level 2 ON
-                    if(Pin_No == Level3 && Pin_Value ==1) {Serial.print("Level 3");myTouch.Level3_ON();}    //  Level 3 ON
-                    if(Pin_No == Level4 && Pin_Value ==1) {Serial.print("Level 4");myTouch.Level4_ON();}    //  Level 4 ON
-                  }
-                  
-                  else
-                    digitalWrite(Pin_No,Pin_Value); break;                // Relay ON/OFF
-                    
-          case(222):myTouch.FAN_OFF();              // FAN OFF
-                    //Serial.print("FAN OFF"); 
-                    break;
-               
-          case(111):
-                  myTouch.Master_ON(); 
-                 //Serial.println("Master ON"); 
-                  break;
-          case(999):
-                  myTouch.Master_OFF();
-                  //Serial.println("Master OFF"); 
-                  break; 
-  
-        }     
- 
-    }   
+        if(myHBT.read() =='H')                      // detects endline character "H"
+        {
+            //Serial.print("I am in if.read()");
+            //if Pin Type is 0 means Digital Output      
+           switch(Pin_Type)
+           {
+              case(0):
+                      if((Pin_No == Level1 || Pin_No == Level2 || Pin_No == Level3 || Pin_No == Level4) && (Pin_Value == 1))    // Any one Level is selected and Value set to HIGH 
+                      { //Serial.print("In Case 0");
+                        if(Pin_No == Level1 && Pin_Value ==1){ /*Serial.print("Level 1");*/myTouch.FAN_ON();}       //  Level 1 ON 
+                        if(Pin_No == Level2 && Pin_Value ==1){ /*Serial.print("Level 2");*/myTouch.Level2_ON();}    //  Level 2 ON
+                        if(Pin_No == Level3 && Pin_Value ==1) {/*Serial.print("Level 3");*/myTouch.Level3_ON();}    //  Level 3 ON
+                        if(Pin_No == Level4 && Pin_Value ==1) {/*Serial.print("Level 4");*/myTouch.Level4_ON();}    //  Level 4 ON
+                      }
+                      
+                      else
+                        digitalWrite(Pin_No,Pin_Value); break;                // Relay ON/OFF
+                        
+              case(222):myTouch.FAN_OFF();              // FAN OFF
+                        //Serial.print("FAN OFF"); 
+                        break;
+                   
+              case(111):
+                      myTouch.Master_ON(); 
+                     //Serial.println("Master ON"); 
+                      break;
+              case(999):
+                      myTouch.Master_OFF();
+                      //Serial.println("Master OFF"); 
+                      break; 
+              
+            }     
+     
+        }   
     load_output_status = myTouch.readLoadstatus();myHBT.print("B:"); 
     //delay(1000);
     myHBT.print(load_output_status);  //Output Status To update into Smartphone app to display current status of lights
     //getStatus.updateStatus(load_output_status); 
+   //resetFunc();      //cal reset 
+   //Serial.println("Device Reset");
         
       
    }
    if(touch_input ^ prev_touch_input )
   {
-    //when BLE is not connected and signals coming from IR Remote and Capsense Board
-    //Serial.print("Prev Touch:");Serial.println(prev_touch_input,HEX);
-    //Serial.print("Touch_input:");Serial.println(touch_input,HEX);
-    
-    load_output_status = myTouch.readLoadstatus();      // Output status of Loads
-   // Serial.print("Load_Status:");Serial.println(load_output_status,HEX);
-    
-    temp=touch_input;       // current status byte 
-    change = prev_touch_input ^ touch_input;    //if there is a change in state then o/p = !0 
-   // Serial.print("Temp:");Serial.println(temp);
-    new_state    = change & load_output_status;
-    myTouch.cap_setLoad(temp);          //switch as per new_state
-    
-    prev_touch_input = touch_input;
-
-    output_TouchStatus = myTouch.readLoadstatus();myHBT.print("T:"); 
-    myHBT.print(output_TouchStatus);  //Output Status0 To update into Smartphone app to display current status of lights
+      //when BLE is not connected and signals coming from IR Remote and Capsense Board
+      //Serial.print("Prev Touch:");Serial.println(prev_touch_input,HEX);
+      //Serial.print("Touch_input:");Serial.println(touch_input,HEX);
+      
+      load_output_status = myTouch.readLoadstatus();      // Output status of Loads
+     // Serial.print("Load_Status:");Serial.println(load_output_status,HEX);
+      
+      temp=touch_input;       // current status byte 
+      change = prev_touch_input ^ touch_input;    //if there is a change in state then o/p = !0 
+     // Serial.print("Temp:");Serial.println(temp);
+      new_state    = change & load_output_status;
+      myTouch.cap_setLoad(temp);          //switch as per new_state
+      
+      prev_touch_input = touch_input;
+  
+      output_TouchStatus = myTouch.readLoadstatus();myHBT.print("T:"); 
+      myHBT.print(output_TouchStatus);  //Output Status0 To update into Smartphone app to display current status of lights
     
   }
  
 /// RF Modules Inputs Conditions
 
-Curr_RFStatus = PINB & 0x0F;      //Read RF Status
 
-if( Curr_RFStatus ) // Detecting State change
+if( Curr_RFStatus  ) // Detecting State change
 {
   delay(500);       // Debounce delay
+  myHBT.print("R:");myHBT.println(Curr_RFStatus);
   switch(Curr_RFStatus)
   {
-    case(0x01) :
+    case(0x04) :    //4
+                 myHBT.println("Lamp1");
                  PORTC ^= _BV(PC6) ;   //PC6 Load 1
                  break;
                 
-    case(0x04) : PORTB ^=  _BV(PB6);break;  //PB6 Load 2
-    case(0x05) : PORTB ^=  _BV(PB5);break;  // PB5 Load 3  
-    case(0x08) : PORTB ^=  _BV(PB4);break;  //PB4 Socket Load 4
-    case(0x0A) :  // FAN ON default level to Level 1
+    case(0x02) : PORTB ^=  _BV(PB6);break;  //PB6 Load 2   //////2
+    case(0x06) : PORTB ^=  _BV(PB5);break;  // PB5 Load 3  /////6
+    case(0x01) : PORTB ^=  _BV(PB4);break;  //PB4 Socket Load 4  ////// 1
+    case(0x09) :  // FAN ON default level to Level 1  ///////////9
                
                 //PORTB |= (1<<PB7);    //Level1 ON
                 //PORTD &= (0<<PD4);    //Level 2 OFF
@@ -222,8 +207,8 @@ if( Curr_RFStatus ) // Detecting State change
                 }
                 break;
 
-    case(0x0B) : // Level DOWN
-
+    case(0x0D) : // Level DOWN   ////13
+                myHBT.println("Down");
                 if((fon == true && up_in == true) && (level > 0 && level <= 4))
                 {
                   
@@ -241,7 +226,7 @@ if( Curr_RFStatus ) // Detecting State change
                 }
                                 
                 break;
-    case(0x07) : //Level UP
+    case(0x0E) : //Level UP /////////14
                                 
                 if(fon == true && level <=3)
                 {
@@ -260,7 +245,7 @@ if( Curr_RFStatus ) // Detecting State change
               break;
             
     
-    case(0x0E) : // FAN OFF
+    case(0x0B) : // FAN OFF  //////////11
 
               PORTB &= ~ _BV(PB7);    //Level1 OFF
               PORTD &= ~ _BV(PD4);    //Level 2 OFF
